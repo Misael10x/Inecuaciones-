0e76198356610f4b9431d3555d8e8894292c0a2d
Misael10x <91623505+Misael10x@users.noreply.github.com>
2021-10-07 23:42:05 -0500

    Add files via upload

--- Inecuaciones.m
+++ Inecuaciones.m
@@ -7,8 +7,9 @@
 %Usage                  :C:\Users\MASTER\Pictures\Octave Proyecto
 "inecuaciones de Primer grado"
 syms x
-solve (7x+5<2x-10)
-solve 
+solve (5+x=9)
+solve (x=9-5)
+solve (x=4)
 "Inecuaciones Segundo grado"
 solve (2x^2=50)
 solve (x^2=3x)
@@ -22,5 +23,14 @@ abs (-1-2<3x<1-2)
 abs (-3<3x<1)
 abs (-1<x<0.5)
 
-subplot (-1,0.5)
+x=-4:1:4;
+y=x'*sin(x);
+plot (x,y)
+
+x=3:0.1:6;
+y=sqrt(2x^2=50);
+plot (x,y)
+
+x=-1:0:0.5;
+y=sqrt(|3x-2|<1);
 plot (x,y)
