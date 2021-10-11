82ea097d71a4e8f0975c6bd66936430fbd2550f3
Jose Manuel Arce <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-11 17:42:46 -0500

    Disp
    es para mostrar

--- Inecuaciones.m
+++ Inecuaciones.m
@@ -5,23 +5,23 @@
 %Date                   :20211007
 %Version                :1
 %Usage                  :C:\Users\MASTER\Pictures\Octave Proyecto
-"inecuaciones de Primer grado"
- 
- 
- syms x
+
+disp('inecuaciones de Primer grado');
+pkg load symbolic
+syms x
+disp'5+x=9'
 solve (5+x=9)
 solve (x=9-5)
 solve (x=4)
- 
- "Inecuaciones Segundo grado"
+disp ('Inecuaciones Segundo grado')
+
 solve (2x^2=50)
 solve (x^2=3x)
 solve (5x^2-2x+1=0)
 solve (7x-x^2=0)
 solve (3x*x-5*=6x)
- 
- "Inecuaciones de valor absoluto"
 
+disp('Inecuaciones de valor absoluto')
 abs (|3x-2|<1)
 abs (-1<3x-2<1)
 abs (-1-2<3x<1-2)
